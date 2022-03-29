e1c49181316f6248286dfec8ecb3c42725a74848
mark <markhammond00@gmail.com>
2022-03-28 17:11:51 -0700

    update tutorial key

--- lab1_key.R
+++ lab1_key.R
@@ -24,31 +24,31 @@ needs(car)
 car::leveneTest(vocab ~ instruct, data = df, center = "mean")
 
 # F-ratio
-## calculate the between group variance (i.e., grand mean difference)
-SSE.m <- sum( (df$vocab - mean(df$vocab))^2 )
+## calculate the total group variance (i.e., grand mean difference)
+SST <- sum( (df$vocab - mean(df$vocab))^2 )
 
 ## transform from long to wide data to calculate within group variance
 df_wide <- spread(df, instruct, vocab) %>% 
   clean_names()
 
 ## calculate the within group variance, for each group
-SSE.1 <- (df_wide$physical_science - mean(df_wide$physical_science, na.rm = T))^2
-SSE.2 <- (df_wide$social_science - mean(df_wide$social_science, na.rm = T))^2
+SSW.1 <- (df_wide$physical_science - mean(df_wide$physical_science, na.rm = T))^2
+SSW.2 <- (df_wide$social_science - mean(df_wide$social_science, na.rm = T))^2
 
 ## sum within group variance (sum of squares error)
-SSE <- sum(SSE.1, SSE.2, na.rm = T)
+SSW <- sum(SSW.1, SSW.2, na.rm = T)
 
 ## calculate difference for between group variance (grand mean difference) and group variance
-SSR <- SSE.m-SSE
+SSR <- SST-SSW
 
 ## calculate mean square regression (difference between groups)
-MSR <- SSR/1   # (no. of groups - 1) or 1 degrees of freedom
+MSB <- SSB/1   # (no. of groups - 1) or 1 degrees of freedom
 
 ## calculate mean square error (difference within groups)
-MSE <- SSE/22  # (sample size - no. of groups) or 22 degrees of freedom
+MSW <- SSW/22  # (sample size - no. of groups) or 22 degrees of freedom
 
 ## produce F-ratio statistic 
-Fratio <- MSR/MSE
+Fratio <- MSB/MSW
 
 ## produce p-value for F-ratio
 p.val  <- pf(Fratio, 1, 22, lower.tail=FALSE)
